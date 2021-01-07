b0d22cc7e65ff2e3356ea857732c7828a5bb57fa
SavelevaNastya <71922046+SavelevaNastya@users.noreply.github.com>
2021-01-07 13:41:52 +0700

    Update main.cpp

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <cctype>
-#include "math.h"
-
 
 using namespace std;
 
-
-double func(double x) {
-    return exp(-pow(x, 0.5) + sin(x / 10.0));
+constexpr double func(double x) {
+    return x; 
 }
 
 constexpr double simpson(double(*function)(double), double a, double b, int n) {
-    //static_assert(n <= 0, "n must be >= 0!");
+
     double sum = 0;
     double g = 0;
     double Ai = 0;
@@ -22,10 +19,10 @@ constexpr double simpson(double(*function)(double), double a, double b, int n) {
         Ai = a + i * h;
         Bi = Ai + h;
         g = (function(Ai) + 4.0 * function((Ai + Bi) / 2.0) + function(Bi)) * (Bi - Ai) / 6.0;
-        //static_assert( isdigit(g) == 0, "The value of integral is not a number");
         sum = sum + g;
     }
-    return sum;
+
+    return sum; 
 }
 
 template<typename Meth, typename Function>
@@ -35,20 +32,14 @@ void Integrate(Function func, Meth method, double a, double b, int n) {
 
 int main()
 {
-    constexpr double a = 0;
-	cout << "Set a - lower boundary of integral" << endl;
-	cout << "a = ";
-	//cin >> a;
+    constexpr double a = 0; /*lower boundary of integral*/
+
+    constexpr double b = 1; /*upper boundary of integral*/
 	
-    constexpr double b = 600;
-	cout << "Set b - upper boundary of integral" << endl;
-	cout << "b = ";
-	//cin >> b;
-	
-    constexpr int n = 100;
-    cout << "Set n - nubmer of splits" << endl;
-    cout << "n = ";
-    //cin >> n;
+    constexpr int n = 1000; /*nubmer of splits*/
+
+    static_assert(simpson(func, a, b, n) < 1, "Integral calculated incorrectly");
+    static_assert(simpson(func, a, b, n) > 0, "Integral calculated incorrectly");
 
     Integrate(func, simpson, a, b, n);
 }
