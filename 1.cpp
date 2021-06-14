a5327d37c27eb4060ae8cbd22850019b9ce97c2c
SavelevaNastya <71922046+SavelevaNastya@users.noreply.github.com>
2021-06-14 20:11:20 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,72 +1,75 @@
-//#include <iostream>
-//#include <memory>
-//using namespace std;
-//
-//template<class T>
-//class my_shared_ptr {
-//private:
-//	shared_ptr<T>* ptr;
-//public:
-//	my_shared_ptr() {
-//		ptr = nullptr;
-//	}
-//	
-//	my_shared_ptr(const my_shared_ptr<T>& other)
-//	{
-//		this->ptr = other.ptr;
-//	}
-//
-//	my_shared_ptr(shared_ptr<T>& other)
-//	{
-//		this->ptr = &other;
-//	}
-//	
-//	my_shared_ptr(my_shared_ptr<T>&& other) {
-//		this->ptr = other.ptr;
-//		other.ptr = nullptr;
-//	}
-//	
-//	my_shared_ptr<T>& operator= (my_shared_ptr<T> other) {
-//		my_shared_ptr tmp(other);
-//		return *this;
-//	}
-//
-//	~my_shared_ptr() {
-//
-//	}
-//};
-//class B;
-//
-//class A {
-//
-//public:
-//	A() {
-//		cout << "A - constructor" << endl;
-//	}
-//	~A() {
-//		cout << "A - destructor" << endl;
-//	}
-//
-//	shared_ptr<B> Bptr;
-//};
-//
-//class B {
-//public:
-//	B() {
-//		cout << "B - constructor" << endl;
-//	}
-//	~B() {
-//		cout << "B - destructor" << endl;
-//	}
-//
-//	my_shared_ptr<A> Aptr;
-//};
-//int main()
-//{
-//	auto a = std::make_shared<A>();
-//
-//	a->Bptr = std::make_shared<B>();
-//	a->Bptr->Aptr = a;
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+template<class T>
+class my_shared_ptr {
+private:
+	T* ptr;
+	int* counter;
+public:
+
+	my_shared_ptr() {
+		ptr = nullptr;
+		counter = 0;
+	}
+	my_shared_ptr(T* other) {
+		ptr = other;
+		counter = new int(1);
+	}
+	
+	my_shared_ptr(my_shared_ptr const& other)
+	{
+		if (ptr == other.ptr){
+			++*counter;
+		}
+		else{
+			ptr = other.ptr;
+			counter = other.counter;
+			++*counter;
+		}
+			
+	}
+	
+	my_shared_ptr& operator= (my_shared_ptr const& other) {
+		if (this != &other) {
+			--*counter;
+			ptr = other.ptr;
+			counter = other.counter;
+			++*counter;
+		}
+		return *this;
+	}
+
+	my_shared_ptr& operator=(my_shared_ptr<T>&& other) noexcept
+	{
+		my_shared_ptr(std::move(other)).swap(*this);
+		++*counter;
+		--other.*counter;
+		return *this;
+	}
+
+	~my_shared_ptr() {
+		if (ptr != nullptr && counter == 0) {
+			delete ptr;
+			//cout << "destructor!" << endl;
+		}
+		else {
+			//cout << "destructor!";
+			--*counter;
+		}
+	}
+};
+
+class Dog {
+	int d = 1;
+};
+
+int main()
+{
+	my_shared_ptr<Dog> ptr1 = my_shared_ptr<Dog>(new Dog);
+	my_shared_ptr<Dog> ptr2 = ptr1;
+	my_shared_ptr<Dog> ptr3 = my_shared_ptr<Dog>(new Dog);
+	ptr2 = ptr3;
+
+	return 0;
+}
