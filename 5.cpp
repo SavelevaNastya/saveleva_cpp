4b7d650a59882026802edca7340cf5b5b6bbf5ce
SavelevaNastya <71922046+SavelevaNastya@users.noreply.github.com>
2021-06-14 23:08:24 +0700

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,21 +1,34 @@
-//#include <iostream>
-//using namespace std;
-//
-//double f1(int x, int y) {
-//	return x + y;
-//}
-//
-//double f2(int x, int y) {
-//	return x / y;
-//}
-//
-//template <class A, class B>
-//auto multiply_functions(A func1, B func2) {
-//
-//	const auto func3 = []() {return func1(1, 2) * func2(1, 2); };
-//	return func3;
-//}
-//
-//int main() {
-//	auto func = multiply_functions(f1, f2);
-//}
+#include <iostream>
+using namespace std;
+
+double f1(double a, double b)
+{
+    return a + b;
+}
+
+double f2(double a, double b)
+{
+    return a / b;
+}
+
+template <class Function1, class Function2>
+struct type_traits;
+
+template <class F1, class F2, class In1, class In2>
+struct type_traits<F1(*)(In1, In2), F2(*)(In1, In2)> {
+    using type1 = In1;
+    using type2 = In2;
+};
+
+template<class F1, class F2>
+auto multiply_function(F1 f1, F2 f2)
+{
+    using Type1 = typename type_traits<decltype(f1), decltype(f2)>::type1;
+    using Type2 = typename type_traits<decltype(f1), decltype(f2)>::type2;
+
+    return [=](Type1 a, Type2 b) { return f1(a, b) * f2(a, b); };
+};
+
+int main(){
+    auto f = multiply_function(f1, f2);
+}
