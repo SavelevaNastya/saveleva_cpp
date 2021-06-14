e571b7c775ab9f09c670401610db63295268b1e3
SavelevaNastya <71922046+SavelevaNastya@users.noreply.github.com>
2021-06-14 20:44:40 +0700

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,48 +1,62 @@
-//class MyIntArray {
-//private:
-//	int* data;
-//	size_t length;
-//public:
-//    MyIntArray(int c)
-//    {
-//        data = new int;
-//        *data = c;
-//    }
-//
-//    ~MyIntArray()
-//    {
-//        delete[] data;
-//    }
-//
-//    // Copy constructor
-//    MyIntArray(const MyIntArray& c)
-//        : MyIntArray{ *c.data }
-//    {
-//
-//    }
-//
-//    // Move constructor
-//    MyIntArray(MyIntArray&& c) noexcept
-//        : data{ c.data }
-//    {
-//        c.data = nullptr;
-//    }
-//
-//    // Оператор присваивания перемещением (move assignment)
-//    MyIntArray& operator=(MyIntArray&& c) noexcept
-//    {
-//        if (this == &c)
-//            return *this;
-//        delete[] data;
-//        data = c.data;
-//        c.data = nullptr;
-//        return *this;
-//    }
-//
-//    // operator=
-//    MyIntArray& operator= (const MyIntArray& c) {
-//        data = c.data;
-//    }
-//    
-//
-//};
+#include <utility>
+
+class MyIntArray {
+private:
+	int* data;
+	size_t length;
+public:
+    MyIntArray(int c, size_t l)
+    {
+        data = new int;
+        *data = c;
+        length = l;
+
+    }
+
+    ~MyIntArray()
+    {
+        delete[] data;
+        data = nullptr;
+        length = 0;
+    }
+
+    // Copy constructor
+    MyIntArray(const MyIntArray& c)
+    {
+        length = c.length;
+        data = new int();
+        *data = *c.data;
+
+    }
+
+    // Move constructor
+    MyIntArray(MyIntArray&& c) noexcept
+    {
+        length = c.length;
+        data = std::move(c.data);
+    }
+
+    // Оператор присваивания перемещением (move assignment)
+    MyIntArray& operator=(MyIntArray&& c) noexcept
+    {
+        if (this == &c)
+            return *this;
+
+        delete[] data;
+        data = c.data;
+        length = c.length;
+        c.data = nullptr;
+        return *this;
+    }
+
+    // operator=
+    MyIntArray& operator= (const MyIntArray& c) 
+    {
+        if (this == &c)
+            return *this;
+
+        data = c.data;
+        length = c.length;
+        return *this;
+    }
+};
